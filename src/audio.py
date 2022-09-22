bd154adfbd8b16bbef39eb416cd3ae29c4400155
Raphael Nash <sc20rn@leeds.ac.uk>
2022-09-22 16:55:41 +0100

    Added read conversion functions

--- src/audio.py
+++ src/audio.py
@@ -1,2 +1,33 @@
-# This file contains
+# This file contains functions to open, read from and write to WAVE files
 import wave
+import numpy as np
+from data_conversion import *
+
+def open_file(file):
+    try:
+        return wave.open(file)
+    except:
+        return None
+
+def read(file, t):
+    # Gets byte data from file and converts it to numpy array
+    # file - open Wave_read object
+    # t - length of time in seconds to extract from file
+    NFRAMES = file.getnframes()         # Total number of frames in the file
+    CHANNELS = file.getnchannels()      # Number of audio channels
+    SAMPWIDTH = file.getsampwidth()     # Bytes per sample
+    FRAMERATE = file.getframerate()     # Frames/second
+
+    # Get bytes
+    frames = file.readframes()
+
+    # Convert to binary
+    frames = hexToBinary(frames)
+    
+    # Convert to decimal/float
+    frames = binaryToDec(frames)
+
+    # Store in numpy array
+    frames = np.array(frames)
+
+    return frames
---
+++ src/data_conversion.py
@@ -0,0 +1,58 @@
+# This file contains data conversion functions to convert between bases
+
+def bytes_to_binary(data):
+    # Converts bytes object to binary string
+    hex_to_bin = {
+        '0': '0000',
+        '1': '0001',
+        '2': '0010',
+        '3': '0011',
+        '4': '0100',
+        '5': '0101',
+        '6': '0110',
+        '7': '0111',
+        '8': '1000',
+        '9': '1001',
+        'a': '1010',
+        'b': '1011',
+        'c': '1100',
+        'd': '1101',
+        'e': '1110',
+        'f': '1111',
+    }
+
+    
+    hex_string = data.hex()
+    bin_string = ""
+        
+    # Loop over first and second nibbles of each byte, and append each byte in a binary string format
+    for c in hex_string:
+        bin_string += hex_to_bin[c]
+    
+    return bin_string
+
+def binary_to_decimal(n, big_endian=False, signed=True):
+    # Converts binary string to decimal
+    value = 0
+
+    # Convert to 'big endian' format if originally little endian
+    if big_endian == True:
+        n = n[::-1]
+
+    # Sum over all but last bit
+    for i in range(len(n) - 1):
+        value += int(n[i]) * 2**(i)
+    
+    # Get value of MSB if signed
+    msb = 2**(len(n) - 1)
+    if signed:
+        msb *= -1
+    value += msb
+
+    return value
+
+def decimal_to_binary(data):
+    pass
+
+def binary_to_bytes(data):
+    pass
