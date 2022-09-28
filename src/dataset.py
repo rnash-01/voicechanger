319712e632c4b72bc8ae785cdaec360ff27d6a2b
Raphael Nash <sc20rn@leeds.ac.uk>
2022-09-28 20:03:20 +0100

    make window size and stride parameters
    Make the window size and stride over the audio file parameters
    
    Note: This does not change the window size and stride per spectrogram.
    
    This, instead, modifies how big the examples of the file are.
    
    This also modifies how they overlap.
    
    The spectrograms consider each window as its own data.
    
    It then puts its own window size and stride over it.

--- src/dataset.py
+++ src/dataset.py
@@ -4,9 +4,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from audio import *
 from preprocessing import *
-from hyperparams import *
 
-def create_dataset(filename):
+def create_dataset(filename, win_size, stride):
     
     # Open file and get sample rate and duration
     audio_file = open_file(filename)
@@ -23,23 +22,21 @@ def create_dataset(filename):
     dataset = []
     freq_ubound = int(10000/SAMP_RATE * ((WIN_SIZE * SAMP_RATE)/2))             # Typical human speech shouldn't really be exceeding 10000Hz
     
-    f_win_size_frames = int(FILE_WINDOW_SIZE * SAMP_RATE)
-    f_stride_frames = int(FILE_STRIDE_SIZE * SAMP_RATE)
+    f_win_size_frames = int(win_size * SAMP_RATE)
+    f_stride_frames = int(stride * SAMP_RATE)
     num_examples = (len(frames) - f_win_size_frames)//f_stride_frames + 1
-    num_examples = np.max([num_examples, 0])                                    # if num_examples is negative, that means the window size is 
+    num_examples = np.max([num_examples, 1])                                    # if num_examples is negative, that means the window size is 
                                                                                 # greater than the number of frames
 
     # Loop over audio in FILE_WINDOW_SIZE second intervals
     for i in range(num_examples):
-        lbound = i * int(FILE_STRIDE_SIZE * SAMP_RATE)
+        lbound = i * f_stride_frames
         ubound = int(f_win_size_frames + i * f_stride_frames)
         ubound = np.min([ubound, frames.shape[0]])
         window = frames[lbound:ubound]
     
-        print(lbound, ubound, len(window))
         xf, yf = spectrogram(window, SAMP_RATE, WIN_SIZE, STRIDE)
         dataset.append(yf[:, -freq_ubound:])
-        print(yf.shape)
     
     # Normalize data on a logarithmic scale
     dataset = np.array(dataset)
@@ -47,11 +44,7 @@ def create_dataset(filename):
     mini = diff/(np.exp(8) - 1)                                                 # ^1
     dataset = np.log(dataset + mini)
 
-    return (xf[:freq_ubound], np.array(dataset))
-
-xf, dataset = create_dataset('audio/donkey kong punch.wav')
-for i, yf in enumerate(dataset):
-    plt.imsave(f"spectrogram_real_{i}.png", np.transpose(yf), cmap='inferno')
+    return (xf[-freq_ubound:], np.array(dataset))
 
 # Footnotes
 # 1 the number contained in np.exp(x) denotes how many powers of e the resulting spectrogram values should range over.
